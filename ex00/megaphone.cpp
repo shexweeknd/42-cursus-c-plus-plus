69ed084e3e11f47014d2e6e039d0954171434129
hramaros <hramaros@student.42antananarivo.mg>
2024-11-19 14:08:23 +0300

    megaphone - OK

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,4 +1,24 @@
-int main(void)
-{     
-    return (1);
+#include <iostream>
+
+int main(int arg, char **argv)
+{
+    int i;
+    int j;
+
+    i = 1;
+    while (argv[i])
+    {
+        j = 0;
+        while (argv[i][j])
+        {
+            if (argv[i][j] >= 97 && argv[i][j] <= 122)
+                std::cout << (char)(argv[i][j] - 32);
+            else
+                std::cout << argv[i][j];
+            j++;
+        }
+        i++;
+    }
+    std::cout << std::endl;
+    return (0);
 }
