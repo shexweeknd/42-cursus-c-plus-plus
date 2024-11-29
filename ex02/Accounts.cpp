c6c62d31edd03acedbacf58daea5ea0d91d133b3
hramaros <hramaros@student.42antananarivo.mg>
2024-11-29 12:00:54 +0300

    fullfill template of Account.cpp class

---
+++ ex02/Accounts.cpp
@@ -0,0 +1,64 @@
+Account::Account(int initial_deposit)
+{
+    return ;
+}
+
+Account::~Account(void)
+{
+    return ;
+}
+
+// Public methods
+
+/*------------Accessors--------------*/
+
+static int	Account::getNbAccounts( void )
+{
+    return ;
+}
+
+static int	Account::getTotalAmount( void )
+{
+    return ;
+}
+
+static int	Account::getNbDeposits( void )
+{
+    return ;
+}
+
+static int	Account::getNbWithdrawals( void )
+{
+    return ;
+}
+
+static void	Account::displayAccountsInfos( void )
+{
+    return ;
+}
+
+/*-----------Non-Accessors------------*/
+
+void	Account::makeDeposit( int deposit )
+{
+    return ;
+}
+bool	Account::makeWithdrawal( int withdrawal )
+{
+    return ;
+}
+int		Account::checkAmount( void ) const
+{
+    return ;
+}
+void	Account::displayStatus( void ) const
+{
+    return ;
+}
+
+// Private methods
+
+static void	Account::_displayTimestamp( void )
+{
+    return ;
+}
