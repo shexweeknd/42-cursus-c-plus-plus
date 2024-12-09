f429e27bfae746292a3a4c4a052d762471b96dc2
hramaros <hramaros@student.42antananarivo.mg>
2024-12-09 12:06:40 +0300

    cpp01/ex04     DONE

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+    if (argc != 4 || !argv[1] || !argv[2] || !argv[3])
     {
         std::cerr << "Usage: ./ex04 [filename] [s1] [s2]" << std::endl;
         return (1);
     }
-    std::string filename = argv[1];
-    std::string s1 = argv[2];
-    std::string s2 = argv[3];
+    std::string     s1 = argv[2];
+    std::string     s2 = argv[3];
 
-    //TODO: Implement the replace function of s1 into s2 in the file
+    std::ifstream   ifile(argv[1]);
+    if (!ifile.is_open())
+    {
+        std::cerr << "Error: Could not open file" << std::endl;
+        return (1);
+    }
+
+    std::string     iline;
+    std::ofstream   ofile((std::string(argv[1]) + ".replace").c_str());
+
+    std::size_t     offset = 0;
+    while (std::getline(ifile, iline))
+    {
+        while ((offset = iline.find(s1)) != std::string::npos)
+        {
+            iline.erase(offset, s1.length());
+            iline.insert(offset, s2);
+        }
+        ofile << iline << std::endl;
+    }
+    ofile.close();
+    ifile.close();
     return (0);
 }
