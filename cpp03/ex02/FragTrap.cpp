c16a80eeea995a7920bdf639061d65a2bd685327
hramaros <hramaros@student.42antananarivo.mg>
2024-12-19 11:37:41 +0300

    cpp03/ex02     ClapTrap, ScavTrap and FragTrap FIXED

---
+++ cpp03/ex02/FragTrap.cpp
@@ -0,0 +1,75 @@
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap(void): ClapTrap()
+{
+    _name = "Default";
+    _hitPoints = 100;
+    _energyPoints = 100;
+    _attackDamage = 30;
+    std::cout << "WALAY! Default FragTrap is born!" << std::endl;
+    return ;
+}
+
+FragTrap::FragTrap(std::string name) : ClapTrap(name)
+{
+    this->_name = name;
+    this->_hitPoints = 100;
+    this->_energyPoints = 100;
+    this->_attackDamage = 30;
+    std::cout << "WALAY! FragTrap " << _name << " is born!" << std::endl;
+    return ;
+}
+
+FragTrap::FragTrap(FragTrap const & src): ClapTrap(src)
+{
+    *this = src;
+    return ;
+}
+
+FragTrap &FragTrap::operator=(FragTrap const & src)
+{
+    this->_name = src._name;
+    this->_hitPoints = src._hitPoints;
+    this->_energyPoints = src._energyPoints;
+    this->_attackDamage = src._attackDamage;
+    return (*this);
+}
+
+FragTrap::~FragTrap(void)
+{
+    std::cout << "FragTrap " << _name << " is dead!" << std::endl;
+    return ;
+}
+
+// FragTrap actions
+void FragTrap::attack(std::string const & target)
+{
+    if (this->_energyPoints <= 0)
+    {
+        std::cout << "FragTrap " << _name << " has no energy points and cannot attack!" << std::endl;
+        return ;
+    }
+    else if (this->_hitPoints <= 0)
+    {
+        std::cout << "FragTrap " << _name << " has no hit points and cannot attack!" << std::endl;
+        return ;
+    }
+    std::cout << "FragTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!" << std::endl;
+    return ;
+}
+
+void FragTrap::highFivesGuys(void)
+{
+    if (this->_energyPoints <= 0)
+    {
+        std::cout << "FragTrap " << _name << " has no energy points and cannot high five!" << std::endl;
+        return ;
+    }
+    else if (this->_hitPoints <= 0)
+    {
+        std::cout << "FragTrap " << _name << " has no hit points and cannot high five!" << std::endl;
+        return ;
+    }
+    std::cout << "FragTrap " << _name << ": 'high fives ?'" << std::endl;
+    return ;
+}
