0f712fbab0fff6289d8ee9942faa03bc35884959
hramaros <hramaros@student.42antananarivo.mg>
2025-01-18 10:59:00 +0300

    cpp01/ex04:     CHANGING ALGORITHM STRUCTURE     NEED TO DEBUG THE TEST OF '' ' '

--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -9,8 +9,11 @@ int main(int argc, char **argv)
         std::cerr << "Usage: ./ex04 [filename] [s1] [s2]" << std::endl;
         return (1);
     }
-    std::string     s1 = argv[2];
-    std::string     s2 = argv[3];
+    // std::string     s1 = argv[2];
+    // std::string     s2 = argv[3];
+
+    std::string    s1 = "";
+    std::string    s2 = " ";
 
     std::ifstream   ifile(argv[1]);
     if (!ifile.is_open())
@@ -19,19 +22,22 @@ int main(int argc, char **argv)
         return (1);
     }
 
-    std::string     iline;
     std::ofstream   ofile((std::string(argv[1]) + ".replace").c_str());
 
+    // prendre le contenu du fichier dans un gros string
+    std::string     content;
+    content.assign((std::istreambuf_iterator<char>(ifile)), (std::istreambuf_iterator<char>()));
+
+    // remplacer les occurences de s1 par s2 de maniere recursive
     std::size_t     offset = 0;
-    while (std::getline(ifile, iline))
+    while ((offset = content.find(s1)) != std::string::npos)
     {
-        while ((offset = iline.find(s1)) != std::string::npos)
-        {
-            iline.erase(offset, s1.length());
-            iline.insert(offset, s2);
-        }
-        ofile << iline << std::endl;
+        content.erase(offset, s1.length());
+        content.insert(offset, s2);
     }
+    // ecrire
+    ofile << content;
+
     ofile.close();
     ifile.close();
     return (0);
