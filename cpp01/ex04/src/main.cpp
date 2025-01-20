1dc54b14ec3989d5c773be75b8ff32ade9f82d8b
hramaros <hramaros@student.42antananarivo.mg>
2025-01-20 08:35:19 +0300

    cpp01/ex04:     - TESTING DONE

--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -9,11 +9,8 @@ int main(int argc, char **argv)
         std::cerr << "Usage: ./ex04 [filename] [s1] [s2]" << std::endl;
         return (1);
     }
-    // std::string     s1 = argv[2];
-    // std::string     s2 = argv[3];
-
-    std::string    s1 = "";
-    std::string    s2 = " ";
+    std::string     s1 = argv[2];
+    std::string     s2 = argv[3];
 
     std::ifstream   ifile(argv[1]);
     if (!ifile.is_open())
@@ -30,7 +27,7 @@ int main(int argc, char **argv)
 
     // remplacer les occurences de s1 par s2 de maniere recursive
     std::size_t     offset = 0;
-    while ((offset = content.find(s1)) != std::string::npos)
+    while (s1.length() && ((offset = content.find(s1)) != std::string::npos))
     {
         content.erase(offset, s1.length());
         content.insert(offset, s2);
